c850cfd3820ffc9032a2ea51e6dd43668e002a03
Lance Mitchell <lmitchell@spartanlync.com>
2020-08-07 15:28:36 -0400

    local to remote rSync tests

--- splgeotabmap.src/scripts/utils.js
+++ splgeotabmap.src/scripts/utils.js
@@ -1,14 +1,14 @@
 window.toggle = (el, val) => {
-    el.style.display = val ? "block" : "none";
-}
+   el.style.display = val ? "block" : "none";
+};
 
 window.hexToRGBArray = (hexColor) => {
-    let hexToDec = offset => parseInt(hexColor.replace("#", "").substr(offset, 2), 16);
+   const hexToDec = offset => parseInt(hexColor.replace("#", "").substr(offset, 2), 16);
 
-    return {
-        r: hexToDec(0),
-        g: hexToDec(2),
-        b: hexToDec(4),
-        a: 1
-    };
-}
+   return {
+      r: hexToDec(0),
+      g: hexToDec(2),
+      b: hexToDec(4),
+      a: 1
+   };
+};

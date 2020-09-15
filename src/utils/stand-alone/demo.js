56b1bc9a1f42ff63ddf1d718503d0e08b02b7c35
Lance Mitchell <lmitchell@spartanlync.com>
2020-09-15 13:54:01 -0400

    Exclude historical devices from vehicle search + Added group filter to Veh/Status/Exception API searches

--- src/utils/stand-alone/demo.js
+++ src/utils/stand-alone/demo.js
@@ -3,187 +3,186 @@ import { initDemoData, generatedDevices } from "./demo-data";
 const feedBufferSeconds = 360;
 
 export const fakeUserData = {
-	companyAddress: "fakeCompanyAddress"
+   companyAddress: "fakeCompanyAddress"
 };
 
 export const fakeSession = {
-	userName: "demoUserName",
-	database: "demoDatabase",
-	sessionId: "demoSessionId"
+   userName: "demoUserName",
+   database: "demoDatabase",
+   sessionId: "demoSessionId"
 };
 
 export const typeNameHandlers = {
-	"AddInData": [],
-	"User": [fakeUserData],
+   "AddInData": [],
+   "User": [fakeUserData],
 
-	LogRecord(fromDate, toDate, deviceSearch) {
+   LogRecord(fromDate, toDate, deviceSearch) {
 
-		// const msDiff = new Date() - new Date(fromDate);
-		// const secondsDiff = Math.round(msDiff / 1000);
+      // const msDiff = new Date() - new Date(fromDate);
+      // const secondsDiff = Math.round(msDiff / 1000);
 
-		const results = Object.values(generatedDevices).flatMap(device =>
-			device.getLogRecordForTimeRange(fromDate, toDate)
-		);
+      const results = Object.values(generatedDevices).flatMap(device =>
+         device.getLogRecordForTimeRange(fromDate, toDate)
+      );
 
-		// const totalRecords = Math.min(1000, secondsDiff / 10);
-		// const newID = createNewDevice();
+      // const totalRecords = Math.min(1000, secondsDiff / 10);
+      // const newID = createNewDevice();
 
-		// console.warn('fromDate', createFakeLogRecords(fromDate, 1, 100), secondsDiff);
-		// const { length } = route1;
-		// // console.warn('25', length);
+      // console.warn('fromDate', createFakeLogRecords(fromDate, 1, 100), secondsDiff);
+      // const { length } = route1;
+      // // console.warn('25', length);
 
-		// let currentID = 1;
-		// let currentIndex = 0;
+      // let currentID = 1;
+      // let currentIndex = 0;
 
-		// const results = [generateLogRecord(fromDate, currentIndex, currentID)];
+      // const results = [generateLogRecord(fromDate, currentIndex, currentID)];
 
-		// while (isGetFeed && currentIndex < 12) {
-		// 	results.push(generateLogRecord(fromDate, currentIndex, currentID));
-		// 	currentIndex++;
-		// }
-		return results;
-	},
+      // while (isGetFeed && currentIndex < 12) {
+      // 	results.push(generateLogRecord(fromDate, currentIndex, currentID));
+      // 	currentIndex++;
+      // }
+      return results;
+   },
 
-	ExceptionEvent() {
-		return [];
-	}
+   ExceptionEvent() {
+      return [];
+   }
 };
 
 function getCurrentTime() {
 
-	return new Date().getTime();
+   return new Date().getTime();
 }
 
 export const callNameHandlers = {
 
-	Get(parameters) {
-		const {
-			typeName
-		} = parameters;
+   Get(parameters) {
+      const {
+         typeName
+      } = parameters;
 
-		const result = typeNameHandlers[typeName];
-		return result;
-	},
+      const result = typeNameHandlers[typeName];
+      return result;
+   },
 
-	GetCoordinates() {
-		const y = 43.515228;
-		const x = -79.683523;
-		return [{ x, y }];
-	},
+   GetCoordinates() {
+      const y = 43.515228;
+      const x = -79.683523;
+      return [{ x, y }];
+   },
 
-	GetFeed(parameters) {
-		const {
-			typeName,
-			fromVersion,
-			search
-		} = parameters;
+   GetFeed(parameters) {
+      const {
+         typeName,
+         fromVersion,
+         search
+      } = parameters;
 
-		const {
-			search: {
-				fromDate
-			}
-		} = parameters;
+      const {
+         search: {
+            fromDate
+         }
+      } = parameters;
 
-		// console.warn(92, typeName, fromVersion);
+      // console.warn(92, typeName, fromVersion);
 
-		const result = {
-			data: [],
-			toVersion: fromVersion
-		};
+      const result = {
+         data: [],
+         toVersion: fromVersion
+      };
 
-		const newDataNeeded = fromVersion === undefined || fromVersion < getCurrentTime();
+      const newDataNeeded = fromVersion === undefined || fromVersion < getCurrentTime();
 
-		if (newDataNeeded) {
+      if (newDataNeeded) {
 
-			const newVersion = getCurrentTime() + feedBufferSeconds * 1000;
-			const startingTime = fromVersion ? fromVersion : dateToTime(fromDate);
+         const newVersion = getCurrentTime() + feedBufferSeconds * 1000;
+         const startingTime = fromVersion ? fromVersion : dateToTime(fromDate);
 
-			const typeHandleFunction = typeNameHandlers[typeName];
-			result.data = typeHandleFunction(startingTime, newVersion, search);
+         const typeHandleFunction = typeNameHandlers[typeName];
+         result.data = typeHandleFunction(startingTime, newVersion, search);
 
-			result.toVersion = newVersion;
-		}
+         result.toVersion = newVersion;
+      }
 
-		return result;
-		// if (toVersion === undefined) {
+      return result;
+      // if (toVersion === undefined) {
 
-		// 	const result = {
-		// 		data,
-		// 		toVersion
-		// 	};
+      // 	const result = {
+      // 		data,
+      // 		toVersion
+      // 	};
 
-		// 	return result;
-		// }
+      // 	return result;
+      // }
 
-		// fromVersion += 60000;
+      // fromVersion += 60000;
 
-		// const typeHandleFunction = typeNameHandlers[typeName];
-		// const data = typeHandleFunction(search, true);
+      // const typeHandleFunction = typeNameHandlers[typeName];
+      // const data = typeHandleFunction(search, true);
 
-		// const result = {
-		// 	data,
-		// 	fromVersion
-		// };
+      // const result = {
+      // 	data,
+      // 	fromVersion
+      // };
 
-		// console.warn(105, fromVersion < new Date().getTime());
+      // console.warn(105, fromVersion < new Date().getTime());
 
-		// if (fromVersion < new Date().getTime()) {
-		// 	// console.warn('82', result);
-		// 	return result;
-		// }
+      // if (fromVersion < new Date().getTime()) {
+      // 	// console.warn('82', result);
+      // 	return result;
+      // }
 
-	}
+   }
 };
 
 export const api = {
 
-	call(call, parameters, resolve, reject) {
-		const callHandleFunction = callNameHandlers[call];
-		const result = callHandleFunction(parameters);
-		// console.warn('51', call, parameters, resolve, reject, result);
-		resolve(result);
-	},
+   call(call, parameters, resolve, reject) {
+      const callHandleFunction = callNameHandlers[call];
+      const result = callHandleFunction(parameters);
+      // console.warn('51', call, parameters, resolve, reject, result);
+      resolve(result);
+   },
 
-	multiCall(calls, resolve, reject) {
+   multiCall(calls, resolve, reject) {
 
-		const results = [];
+      const results = [];
 
-		calls.map(eachCall => {
-			// console.warn('70', eachCall);
-			const [callName, parameters] = eachCall;
-			this.call(callName, parameters, callResult => results.push(callResult), reject);
-		});
+      calls.map(eachCall => {
+         // console.warn('70', eachCall);
+         const [callName, parameters] = eachCall;
+         this.call(callName, parameters, callResult => results.push(callResult), reject);
+      });
 
-		// console.warn(154, results);
-		resolve(results);
-	},
+      // console.warn(154, results);
+      resolve(results);
+   },
 
-	getSession(callBack) {
-		callBack(fakeSession);
-	}
+   getSession(callBack) {
+      callBack(fakeSession);
+   }
 
 };
 
 export const geotab = {
 
-	addin: {
+   addin: {
 
-		set realTimeMap(RTM) {
+      set realTimeMap(RTM) {
 
-			initDemoData();
+         initDemoData();
 
-			const { initialize, focus, blur } = RTM();
+         const { initialize, focus, blur } = RTM();
 
-			const state = {};
-			const callback = () => {};
-			document.body.style.height = "100vh";
+         const state = {};
+         const callback = () => { };
+         document.body.style.height = "100vh";
 
-			initialize(api, state, callback);
-
-		}
-	},
+         initialize(api, state, callback);
+      }
+   },
 };
 
 export function dateToTime(date) {
-	return new Date(date).getTime();
+   return new Date(date).getTime();
 }
--- src/utils/stand-alone/geotab.js
+++ src/utils/stand-alone/geotab.js
@@ -1,119 +1,138 @@
 import GeotabApi from "mg-api-js";
 import React from "react";
-import {
-  LoginPage
-} from "./login";
+import { LoginPage } from "./login";
 import ReactDOM from "react-dom";
-
 import storage from "../../dataStore";
 
 let initRTMFunc;
 let isLoginScreenRendered = false;
 
 export const geotabStandAlone = {
-  addin: {
-    set realTimeMap(RTM) {
-      document.body.style.height = "100vh";
-      storage.isStandAlone = true;
+   addin: {
+      set realTimeMap(RTM) {
+         document.body.style.height = "100vh";
+         storage.isStandAlone = true;
 
-      const { initialize, focus, blur } = RTM();
-      initRTMFunc = initialize;
-      _authenticate();
-    }
-  },
+         const { initialize, focus, blur } = RTM();
+         initRTMFunc = initialize;
+         _authenticate();
+      }
+   },
+};
+
+function _mockState() {
+   return {
+      getState: function () {
+         const hash = location.hash;
+         const hashLength = hash.length;
+         return !hashLength ? {} : rison.decode(location.hash.substring(1, location.hash.length));
+      },
+      setState: function (s) {
+         location.hash = Object.keys(s).length ? "#" + rison.encode(s) : "";
+      },
+      gotoPage: function (page, args) { },
+      hasAccessToPage: function (page) {
+         return !!page;
+      },
+      getGroupFilter: function () {
+         return [{
+            id: "GroupCompanyId"
+         }];
+      }
+   };
 };
 
 function _authenticate() {
 
-  const {
-    email = "",
-    password = "",
-    database = "",
-    server = "my.geotab.com"
-  } = _retrieveInputData();
+   const {
+      email = "",
+      password = "",
+      database = "",
+      server = "my.geotab.com"
+   } = _retrieveInputData();
 
-  const api = GeotabApi(authenticateCallback =>
-    authenticateCallback(
-      server,
-      database,
-      email,
-      password,
-      _handleUnsuccessfulLogin
-    )
-  );
+   const api = GeotabApi(authenticateCallback =>
+      authenticateCallback(
+         server,
+         database,
+         email,
+         password,
+         _handleUnsuccessfulLogin
+      )
+   );
 
-  _checkLoginSuccessful(api)
-    .then(api => _handleSuccessfulLogin(api))
-    .catch(_handleUnsuccessfulLogin);
+   _checkLoginSuccessful(api)
+      .then(api => _handleSuccessfulLogin(api))
+      .catch(_handleUnsuccessfulLogin);
 }
 
 function _checkLoginSuccessful(api) {
 
-  return new Promise((resolve, reject) => {
-    const testCall = {
-      typeName: "Device",
-      resultsLimit: 1
-    };
-    api.call("Get", testCall, () => resolve(api), reject);
-  });
+   return new Promise((resolve, reject) => {
+      const testCall = {
+         typeName: "Device",
+         resultsLimit: 1
+      };
+      api.call("Get", testCall, () => resolve(api), reject);
+   });
 };
 
 function _handleSuccessfulLogin(api) {
-  _hideError();
-  initRTMFunc(api, {}, () => { });
+   _hideError();
+   initRTMFunc(api, _mockState(), () => { });
 }
 
 function _handleUnsuccessfulLogin(err) {
-  console.error("61", err);
-  if (!isLoginScreenRendered) {
-    isLoginScreenRendered = true;
-    _createLoginInput();
-  } else {
-    _showError();
-  }
+   console.error("61", err);
+   if (!isLoginScreenRendered) {
+      isLoginScreenRendered = true;
+      _createLoginInput();
+   } else {
+      _showError();
+   }
 }
 
 function _createLoginInput() {
-  ReactDOM.render(
-    <LoginPage
-      handleFormSubmit={_handleLoginClicked}
-    />,
-    document.getElementById("real-time-map-container")
-  );
+   ReactDOM.render(
+      <LoginPage
+         handleFormSubmit={_handleLoginClicked}
+      />,
+      document.getElementById("real-time-map-container")
+   );
 };
 
 function _handleLoginClicked(event) {
-  event.preventDefault();
-  _hideError();
-  _authenticate();
+   event.preventDefault();
+   _hideError();
+   _authenticate();
 };
 
 function _retrieveInputData() {
-  return {
-    email: _getValue("RTM-login-email"),
-    password: _getValue("RTM-login-password"),
-    database: _getValue("RTM-login-database"),
-    server: _getValue("RTM-login-server")
-  };
+   return {
+      email: _getValue("RTM-login-email"),
+      password: _getValue("RTM-login-password"),
+      database: _getValue("RTM-login-database"),
+      server: _getValue("RTM-login-server")
+   };
 };
 
 function _getValue(id) {
-  const elem = document.getElementById(id);
-  if (elem) {
-    return elem.value;
-  }
+   const elem = document.getElementById(id);
+   if (elem) {
+      return elem.value;
+   }
 };
 
 function _showError() {
-  const loginError = document.getElementById("RTM-Login-error");
-  if (loginError) {
-    loginError.style.display = "block";
-  }
+   const loginError = document.getElementById("RTM-Login-error");
+   if (loginError) {
+      loginError.style.display = "block";
+   }
 }
 
 function _hideError() {
-  const loginError = document.getElementById("RTM-Login-error");
-  if (loginError) {
-    loginError.style.display = "none";
-  }
+   const loginError = document.getElementById("RTM-Login-error");
+   if (loginError) {
+      loginError.style.display = "none";
+   }
 }
